819e663ae030bc134803646d606fe201926b40cc
Igor <denlandpud@gmail.com>
2023-02-22 22:16:48 +0300

    task_35

---
+++ task_35/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 35:Задайте одномерный массив из 123 случайных чисел [0: 1000].
+//Найдите коли-во элементов массива, значения которых лежат в отрезке [10,99].
+Console.Clear();
+int[] startArray = GetArray(123, -200 , 1230);
+Console.WriteLine($"Кол-во элементов в отрезке [10;99] = {GetCountElements(startArray, 10, 99)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetCountElements(int[] array, int leftRange, int rigthRange)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item >= leftRange && item <= rigthRange) count++;
+    }
+    return count;
+}
