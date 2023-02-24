b32cd798578e3ab39b5e90b1e7b494503c0719ff
Igor <denlandpud@gmail.com>
2023-02-25 02:07:02 +0300

    task_37

--- task_37/Program.cs
+++ task_37/Program.cs
@@ -3,8 +3,9 @@
 //Результат запишите в новом массиве.
 //[1 2 3 4 5] -> 5 8 3              [6 7 3 6]-> 36 21
 Console.Clear();
-Console.Write("Введите элементы массива через пробел: ");
-string elements = Console.ReadLine();
+
+Console.Write("Enter the array elements separated by a space: ");
+string elements = Console.ReadLine()!;
 int[] baseArray = GetArrayFromString(elements);
 
 Console.WriteLine(String.Join(" ", ResultArray(baseArray)));
@@ -13,7 +14,6 @@ int[] GetArrayFromString(string stringArray)
 {
     string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
     int[] res = new int[nums.Length];
-
     for (int i = 0; i < nums.Length; i++)
     {
         res[i] = int.Parse(nums[i]);
@@ -27,8 +27,7 @@ int[] ResultArray(int[] array)
 //    if (array.Length % 2 == 1) size++;
     int[] result = new int[size];
     for (int i = 0; i < array.Length / 2; i++)
-    {
-        
+    {        
         result[i] = array[i] * array[array.Length - 1 - i];
     }
     if (array.Length % 2 == 1) result[size-1] = array[array.Length / 2];
